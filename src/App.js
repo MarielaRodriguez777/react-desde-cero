a97c0e05e10a091cdad8a2ab5e5e9da8aa1cd098
MarielaRodriguez777 <mariela_rodriguez52@yahoo.es>
2021-05-11 11:04:29 -0600

    Primer commit del curso React.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./styles/styles.scss"
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    /*<Fragment>
+      <div className="saludo">
+        <h1>Hola Mundo</h1>
+        <p>Hola, saludando desde mi componente</p>
+      </div>
+      <div>
+        <img src="https://ed-grid.com/assets/img/utils/rwd.svg"/>
+      </div>
+    </Fragment>*/
+    <
+    >
+    <
+    div className = "main-banner img-container l-section"
+    id = "main-banner" >
+    <
+    div className = "ed-grid lg-grid-6" >
+    <
+    div className = "lg-cols-4 lg-x-2" >
+    <
+    img className = "main-banner__img"
+    alt = "foto"
+    src = "https://wallpapercave.com/wp/wp5852521.jpg" / >
+    <
+    div className = "main-banner__data s-center" >
+    <
+    p className = "t2 s-mb-0" > Cursos de EDteam < /p> <
+    p > Como sea < /p> <
+    a href = "https://ed.team"
+    className = "button" > Like and Suscribe < /a> <
+    /div> <
+    /div> <
+    /div> <
+    /div>
+
+    <
+    div className = "ed-grid m-grid-3" >
+    <
+    Curso / >
+    <
+    Curso / >
+    <
+    Curso / >
+    <
+    Curso / >
+    <
+    Curso / >
+    <
+    Curso / >
+    <
+    Curso / >
+    <
+    Curso / >
+    <
+    /div> <
+    />
+)
 
 export default App;
+
+
+/** Reglas JSX
+ * Toda etiqueta debe cerrarse
+ * los componentes deben devolver UN solo elemento padre
+ * Apoyarse de los fragments cuando necesito devolver 2 elementos
+ * Fragment => <> hijos </>
+ * img siempre se cierra
+ */
